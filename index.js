92d5f702f40eee6e7ee73aaf4de1cb9bcdf0a962
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-11-20 22:38:47 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x) {
+
+  if (x > 2500) {
+    return "No can do.";
+  } else if (x > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (x >= 400 && x <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (x <= 400) {
+    return "This one is on me!";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(y){
+  switch(y) {
+    case "generous" :
+      return "Thank you so much."
+    case "not as generous" :
+      return "Thank you."
+    default :
+      return "Bye."
+  }
+
+  
   // Write your code here!
 }
+
+scuberGreetingForFeet (600);
